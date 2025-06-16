2f2faf3a3c4955d9c33c67e0c8b7a7181645e6ac
mark-mccorkell <djmmccorkell@gmail.com>
2025-06-16 22:41:44 +0100

    Add user-inputted prompt

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import os
-from dotenv import load_dotenv
+import sys
 from google import genai
+from dotenv import load_dotenv
 
+def main():
+    load_dotenv()
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+    args = sys.argv[1:]
 
-client = genai.Client(api_key=api_key)
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "hHow do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=user_prompt
+    )
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
